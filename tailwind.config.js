1637b60f1b1e28e71ec26ffc5b74646966021126
dariusp0p <dariuspop2005@gmail.com>
2026-03-24 22:31:40 +0200

    chore: updated project configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#102542",
+        cyan: "#087F8C",
+        background: "#FFF0EB",
+        orange: "#EF8354",
+      },
+    },
+  },
+  plugins: [],
+};
